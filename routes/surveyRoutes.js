c6da7ff7b58ec2cf5816d92dd86a14949e1eb792
Nick Sherman <ogleffeckt2@yahoo.com>
2020-01-29 20:19:13 -0500

    Revert "fixing issues"
    This reverts commit 494ab5924827d9cb5e23d5082bf68e7452e65186.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,18 +1,6 @@
-const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
-const Survey = mongoose.model('surveys');
-
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-		const { title, subject, body, recipients } = req.body;
-
-		const survey = new Survey({
-			title,
-			subject,
-			body,
-			
-		})
-	});
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
 };
