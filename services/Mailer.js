8b374b085f342fcde5fc9f4e67ed7f43e897ebd2
Nick Sherman <ogleffeckt2@yahoo.com>
2020-01-30 14:41:29 -0500

    added onto boilerplate for sending emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,6 +9,25 @@ class Mailer extends helper.Mail {
 		this.from_email = new helper.Email('no-reply@emailexpress.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
+		this.recipients = this.formatAddresses(recipients);
+
+		this.addContent(this.body);
+		this.addClickTracking();
+		this.addRecipients();
+	}
+
+	formatAddresses(recipients) {
+		return recipients.map(({ email }) => {
+			return new helper.Email(email);
+		});
+	}
+
+	addClickTracking() {
+		const trackingSettings = new helper.TrackingSettings();
+		const clickTracking = new helper.ClickTracking(true, true);
+
+		trackingSettings.setClickTracking(clickTracking);
+		this.addTrackingSettings(trackingSettings);
 	}
 }
 
