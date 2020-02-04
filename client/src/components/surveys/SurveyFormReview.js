7bd7223328511323aad7cc45d58cf5cfcc605cf6
Nick Sherman <ogleffeckt2@yahoo.com>
2020-02-03 23:25:03 -0500

    dumped form values, fixed prop names, posted to surveys & redirected on submit

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,30 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+	const reviewFields = _.map(formFields, ({ name, label }) => {
+		return (
+			<div key={name}>
+				<label>{label}</label>
+				<div>{formValues[name]}</div>
+			</div>
+		);
+	});
+
 	return (
 		<div>
 			<h5>Please confirm your entries</h5>
-			<button className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
+			{reviewFields}
+			<button className="red white-text btn-flat white-text" onClick={onCancel}>
 				Back
+			</button>
+			<button onClick={() => submitSurvey(formValues, history)} className="green btn-flat right white-text">
+				Send Survey
+				<i className="material-icons right">email</i>
 			</button>
 		</div>
 	);
@@ -16,4 +34,4 @@ function mapStateToProps(state) {
 	return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
