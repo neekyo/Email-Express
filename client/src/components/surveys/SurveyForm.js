f40ed331546a2cd5cd4cabecad6cb24aa584ba09
Nick Sherman <ogleffeckt2@yahoo.com>
2020-02-03 19:10:10 -0500

    refactoring of survey form & some styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,29 +1,49 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+
+const FIELDS = [
+	{ label: 'Survey Title', name: 'title' },
+	{ label: 'Subject Line', name: 'subject' },
+	{ label: 'Email Body', name: 'body' },
+	{ label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
 	renderFields() {
-		return (
-			<div>
-				<Field label="Survey Title" type="text" name="title" component={SurveyField} />
-				<Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-				<Field label="Email Body" type="text" name="body" component={SurveyField} />
-				<Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-			</div>
-		);
+		return _.map(FIELDS, ({ label, name }) => {
+			return <Field key={name} component={SurveyField} type="text" label={label} name={name} />;
+		});
 	}
+
 	render() {
 		return (
 			<div>
 				<form onSubmit={this.props.handleSubmit((values) => console.log(values))}>
 					{this.renderFields()}
-					<button type="submit">Submit</button>
+					<Link to="/surveys" style={styles.cancel} className="red btn-flat white-text">
+						Cancel
+					</Link>
+					<button type="submit" style={styles.submit} className="btn-flat right white-text">
+						Next
+						<i className="material-icons right">done</i>
+					</button>
 				</form>
 			</div>
 		);
 	}
 }
+
+let styles = {
+	submit: {
+		backgroundColor: '#9F7BC1'
+	},
+	cancel: {
+		textDecoration: 'none'
+	}
+};
 
 export default reduxForm({
 	form: 'surveyForm'
