af924eefe1f4bc433dee49f73c39a2a3814f25fc
Nick Sherman <ogleffeckt2@yahoo.com>
2020-02-08 01:51:11 -0500

    testing prod in heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,8 +8,14 @@ module.exports = (app) => {
 		})
 	);
 
-	app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+	app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
 		res.redirect('/surveys');
+	});
+
+	app.get('/auth/facebook', passport.authenticate('facebook'));
+
+	app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), (req, res) => {
+		res.redirect('/dashboard');
 	});
 
 	app.get('/api/logout', (req, res) => {
